0c21958cd76328b001afc17a2e9ec202cfbf02a4
Gary <hai.alphacreative@gmail.com>
2025-05-17 15:50:47 +0700

    upload source code

---
+++ assets/js/index/script.js
@@ -0,0 +1,14 @@
+import { preloadImages } from "../../libs/utils.js";
+let lenis;
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+};
+preloadImages("img").then(() => {
+  // Once images are preloaded, remove the 'loading' indicator/class from the body
+
+  init();
+});
+$(window).on("beforeunload", function () {
+  $(window).scrollTop(0);
+});
