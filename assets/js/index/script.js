f6f580bb5259d30b81f7e563c3cd2f8ce1f98546
HuuBang11 <huubang0312@gmail.com>
2025-05-20 11:16:38 +0700

    update section branding

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -26,15 +26,13 @@ function customDropdown() {
       e.stopPropagation();
       const $item = $(this);
       let tmpText = $textDropdown.text();
-      const tmpImgSrc = $textDropdown.find("img").attr("src"); // Get the current image src if present
-      const $img = $item.find("img"); // Check if the clicked item contains an img
+      const tmpImgSrc = $textDropdown.find("img").attr("src");
+      const $img = $item.find("img");
 
-      // Swap text content
       $textDropdown.text($item.text());
 
-      // If the item has an image, swap the img src
       if ($img.length) {
-        $textDropdown.html($item.html()); // Swap the entire HTML, including the img
+        $textDropdown.html($item.html());
 
         if ($item.hasClass("language__item")) {
           tmpText = `<span>${tmpText}</span>`;
@@ -64,9 +62,106 @@ function customDropdown() {
   });
 }
 
+function sectionSpecialize() {
+  if ($(".section-specialize").length < 1) return;
+
+  gsap.fromTo(
+    ".section-specialize .image",
+    { scale: 0.5 },
+    {
+      scale: 1,
+      scrollTrigger: {
+        trigger: ".section-specialize",
+        start: "center bottom",
+        end: "bottom bottom",
+        scrub: true
+      },
+      ease: "none"
+    }
+  );
+
+  gsap.to(".specialize-main-slider", {
+    xPercent: -160,
+    scrollTrigger: {
+      trigger: ".section-specialize",
+      start: "top 20%",
+      end: "bottom 20%",
+      scrub: true,
+      // markers: true,
+      onEnter: () => {
+        $(".section-specialize").addClass("theme-light");
+        $("main").addClass("theme-light");
+      },
+      onLeaveBack: () => {
+        $("main").removeClass("theme-light");
+        $(".section-specialize").removeClass("theme-light");
+      }
+    },
+    ease: "none"
+  });
+
+  let hasCounted = false;
+  ScrollTrigger.create({
+    trigger: ".section-specialize",
+    start: "top 15%",
+    once: true,
+    onEnter: () => {
+      if (!hasCounted) {
+        activeNumberCount();
+        hasCounted = true;
+      }
+    }
+  });
+
+  $(".section-specialize .number").each(function () {
+    const $stat = $(this);
+    const patt = /(\D+)?(\d+(\.\d+)?)(\D+)?/;
+    let result = patt.exec($stat.text());
+
+    if (!result) return;
+
+    result.shift();
+    result = result.filter((res) => res != null);
+
+    $stat.empty();
+
+    result.forEach((res) => {
+      if (isNaN(res)) {
+        $stat.append(`<span>${res}</span>`);
+      } else {
+        for (let i = 0; i < res.length; i++) {
+          $stat.append(`
+            <span data-value="${res[i]}">
+              <span>&nbsp;</span>
+              ${Array(parseInt(res[i]) + 1)
+                .join(0)
+                .split(0)
+                .map((x, j) => `<span>${j}</span>`)
+                .join("")}
+            </span>
+          `);
+        }
+      }
+    });
+  });
+
+  function activeNumberCount() {
+    $(".section-specialize .number").each(function () {
+      const ticks = $(this).find("span[data-value]");
+      ticks.each(function () {
+        const dist = parseInt($(this).attr("data-value")) + 1;
+        $(this).css("transform", `translateY(-${dist * 100}%)`);
+      });
+    });
+  }
+}
+
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
   customDropdown();
+  sectionSpecialize();
+
+  ScrollTrigger.refresh();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
