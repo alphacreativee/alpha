e317c392fa06e06cbcaf04a4dc62f7c80d2b97c2
Gary <hai.alphacreative@gmail.com>
2025-06-09 11:01:40 +0700

    add style

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -6,7 +6,7 @@ const lenis = new Lenis({
   duration: 1.4,
   easing: (t) => 1 - Math.pow(1 - t, 4),
   smooth: true,
-  smoothTouch: false
+  smoothTouch: false,
 });
 
 lenis.on("scroll", ScrollTrigger.update);
@@ -49,7 +49,7 @@ function handlePageVisibilityAndFavicon() {
     isBlinking = true;
     const favicons = [
       "./assets/images/use/favicon-gold.svg",
-      "./assets/images/use/favicon-black.svg"
+      "./assets/images/use/favicon-black.svg",
     ];
     let faviconIndex = 0;
 
@@ -214,8 +214,8 @@ function effectText() {
                 trigger: element,
                 start: "top 60%",
                 end: "bottom 60%",
-                toggleActions: "play none none none"
-              }
+                toggleActions: "play none none none",
+              },
             });
           } else {
             // Auto-play case
@@ -224,18 +224,18 @@ function effectText() {
               yPercent: 100,
               opacity: 0,
               stagger: 0.1,
-              ease: "expo.out"
+              ease: "expo.out",
             });
 
             // Play animation immediately after fonts are loaded
             gsap.to(splitTitle, {
               timeScale: 0.2,
-              onStart: () => splitTitle.play(0)
+              onStart: () => splitTitle.play(0),
             });
           }
 
           return splitTitle;
-        }
+        },
       });
     });
 
@@ -246,19 +246,19 @@ function effectText() {
         {
           "will-change": "opacity, transform",
           opacity: 0,
-          y: 20
+          y: 20,
         },
         {
           scrollTrigger: {
             trigger: element,
             start: "top 75%",
-            end: "bottom 75%"
+            end: "bottom 75%",
             // markers: true,
           },
           opacity: 1,
           y: 0,
           duration: 0.3,
-          ease: "sine.out"
+          ease: "sine.out",
         }
       );
     });
@@ -270,14 +270,14 @@ function effectText() {
           {
             "will-change": "opacity, transform",
             opacity: 0,
-            y: 20
+            y: 20,
           },
           {
             opacity: 1,
             y: 0,
             duration: 0.3,
             ease: "sine.out",
-            delay: 0.5 // Độ trễ để tạo hiệu ứng lần lượt
+            delay: 0.5, // Độ trễ để tạo hiệu ứng lần lượt
           }
         );
       });
@@ -289,7 +289,7 @@ function effectText() {
       let splitBlur = SplitText.create(elementBlur, {
         type: "words, chars",
         charsClass: "split-char",
-        wordsClass: "split-word"
+        wordsClass: "split-word",
       });
       gsap.fromTo(
         splitBlur.chars,
@@ -297,7 +297,7 @@ function effectText() {
           filter: "blur(10px) ",
           y: 10,
           willChange: "filter, transform",
-          opacity: 0
+          opacity: 0,
         },
         {
           ease: "none",
@@ -309,9 +309,9 @@ function effectText() {
             trigger: elementBlur.classList.contains("footer-effect-text")
               ? ".footer-ovl"
               : elementBlur,
-            start: "top 90%"
+            start: "top 90%",
             // markers: true,
-          }
+          },
         }
       );
     });
@@ -384,9 +384,9 @@ function introChess() {
       scrub: 1,
       trigger: "#canvas-chess",
       start: "top+=100 bottom",
-      end: "bottom top"
+      end: "bottom top",
     },
-    onUpdate: render
+    onUpdate: render,
   });
 
   // Hiệu ứng cho section-intro-content
@@ -400,7 +400,7 @@ function introChess() {
   const splitContent = new SplitText(contentElement, {
     type: "words,lines",
     mask: "lines",
-    linesClass: "line"
+    linesClass: "line",
   });
 
   // Tạo timeline cho hiệu ứng vào và ngược lại
@@ -409,8 +409,8 @@ function introChess() {
       trigger: "#canvas-chess",
       start: `top+=${(110 / frameCount) * 100 - 20}% top`,
       end: `top+=${(110 / frameCount) * 100 + 20}% top`,
-      toggleActions: "play none none reverse"
-    }
+      toggleActions: "play none none reverse",
+    },
   });
   tl.fromTo(sectionIntroContent, { opacity: 0 }, { opacity: 1, duration: 0.4 });
   // Thêm hiệu ứng cho tag
@@ -462,9 +462,9 @@ function introChess() {
       start: "top top",
       end: "bottom top",
       pin: true,
-      pinSpacing: false
+      pinSpacing: false,
       // markers: true,
-    }
+    },
   });
 }
 
@@ -556,7 +556,7 @@ function whyChooseUs() {
           .querySelector(".header-menu-container")
           .classList.remove("theme-light");
         document.querySelector("main").classList.remove("theme-light");
-      }
+      },
     });
   }
 }
@@ -587,37 +587,37 @@ function coreValue() {
   gsap.fromTo(
     ".core-value .image",
     {
-      clipPath: initialClipPath
+      clipPath: initialClipPath,
     },
     {
       scrollTrigger: {
         trigger: ".core-value .core-value__top",
         start: "top 70%",
         end: "bottom 70%",
-        scrub: 1
+        scrub: 1,
         // markers: true
       },
       clipPath: "inset(0% 0% 0% 0%)", // hiện dần ra
       duration: 0.4,
-      ease: "power2.out"
+      ease: "power2.out",
     }
   );
 
   gsap.fromTo(
     ".core-value .image img",
     {
-      scale: 1.2
+      scale: 1.2,
     },
     {
       scrollTrigger: {
         trigger: ".core-value .core-value__top",
         start: "top 70%",
         end: "bottom 70%",
-        scrub: 1
+        scrub: 1,
       },
       scale: 1.1,
       duration: 0.4,
-      ease: "power2.out"
+      ease: "power2.out",
     }
   );
 
@@ -632,21 +632,21 @@ function coreValue() {
       end: "+=200%",
       scrub: true,
       pin: true,
-      toggleClass: { targets: ".core-value", className: "active" }
+      toggleClass: { targets: ".core-value", className: "active" },
       // markers: true
-    }
+    },
   });
 
   tl.to("#core-value-text", {
     scale: 1.7,
     duration: 0.6,
-    ease: "power2.out"
+    ease: "power2.out",
   });
 
   tl.to("#core-value-text", {
     xPercent: -300,
     duration: 0.5,
-    ease: "none"
+    ease: "none",
   });
 }
 
@@ -657,7 +657,7 @@ function magicCursor() {
 
   gsap.set(circle, {
     xPercent: -50,
-    yPercent: -50
+    yPercent: -50,
   });
 
   let mouseX = 0,
@@ -671,7 +671,7 @@ function magicCursor() {
     gsap.to(circle, {
       x: mouseX,
       y: mouseY,
-      duration: 0.1 // Không có độ trễ
+      duration: 0.1, // Không có độ trễ
     });
   });
 
@@ -704,7 +704,7 @@ function magicCursor() {
         yPercent: -50,
         xPercent: -50,
         duration: 0.3,
-        ease: "power2.out"
+        ease: "power2.out",
       });
     });
 
@@ -716,7 +716,7 @@ function magicCursor() {
         yPercent: -50,
         xPercent: -50,
         duration: 0.3,
-        ease: "power2.out"
+        ease: "power2.out",
       });
     });
   });
@@ -760,19 +760,19 @@ function pinSectionBanner() {
             gsap.to(bannerTitle, {
               opacity: 0,
               duration: 0.5,
-              ease: "power2.out"
+              ease: "power2.out",
             });
             isTitleHidden = true;
           } else if (self.progress < 1 && isTitleHidden) {
             gsap.to(bannerTitle, {
               opacity: 1,
               duration: 0.5,
-              ease: "power2.out"
+              ease: "power2.out",
             });
             isTitleHidden = false;
           }
-        }
-      }
+        },
+      },
     });
   }
 }
@@ -831,7 +831,7 @@ function loading() {
   let tlLoading = gsap.timeline({
     onComplete: () => {
       $("body").removeClass("overflow-hidden");
-    }
+    },
   });
   let loading = $(".loading");
   let body = $("body");
@@ -844,22 +844,22 @@ function loading() {
         loading.find(".loading-logo"),
         {
           opacity: 0,
-          y: -60
+          y: -60,
+          duration: 0.8,
         },
-        0.5
+        0
       )
-
       .to(
         loading.find(".loading-wrapper"),
         {
           scaleY: 0,
-          duration: 1.5
+          duration: 1.5,
         },
-        0.75
+        0.8
       )
       .to(loading, {
         autoAlpha: 0,
-        duration: 0
+        duration: 0,
       });
   } else {
     gsap.delayedCall(4.25, effectTextBanner);
@@ -868,31 +868,31 @@ function loading() {
         loading.find(".loading-logo"),
         {
           opacity: 0,
-          y: -60
+          y: -60,
         },
         0.5
       )
       .to(loading.find(".loading-text"), {
         opacity: 1,
-        y: 0
+        y: 0,
       })
       .to(
         loading.find(".loading-text"),
         {
           opacity: 0,
-          y: -60
+          y: -60,
         },
         2
       )
       .to(loading.find(".loading-desc"), {
         opacity: 1,
-        y: 0
+        y: 0,
       })
       .to(
         loading.find(".loading-desc"),
         {
           opacity: 0,
-          y: -60
+          y: -60,
         },
         3
       )
@@ -901,13 +901,13 @@ function loading() {
         loading.find(".loading-wrapper"),
         {
           scaleY: 0,
-          duration: 1.5
+          duration: 1.5,
         },
         3.25
       )
       .to(loading, {
         autoAlpha: 0,
-        duration: 0
+        duration: 0,
       });
   }
 }
@@ -933,32 +933,32 @@ function effectTextBanner() {
           self.lines,
           {
             yPercent: 100,
-            opacity: 0 // Start from opacity 0
+            opacity: 0, // Start from opacity 0
           },
           {
             yPercent: 0,
             opacity: 1, // Animate to opacity 1
             duration: 0.4,
             stagger: 0.1,
-            ease: "expo.out"
+            ease: "expo.out",
           }
         );
 
         // Play animation immediately after fonts are loaded
         gsap.to(splitTitle, {
           timeScale: 0.2,
-          onStart: () => splitTitle.play(0)
+          onStart: () => splitTitle.play(0),
         });
 
         // Set parent element opacity to 1 after animation starts
         gsap.to(element, {
           opacity: 1,
           duration: 0, // Instant change
-          delay: 0.1 // Slight delay to ensure lines are visible
+          delay: 0.1, // Slight delay to ensure lines are visible
         });
 
         return splitTitle;
-      }
+      },
     });
   });
 
@@ -972,14 +972,14 @@ function effectTextBanner() {
       {
         "will-change": "opacity, transform",
         opacity: 0,
-        y: 20
+        y: 20,
       },
       {
         opacity: 1,
         y: 0,
         duration: 0.3,
         ease: "sine.out",
-        delay: 0.5 + additionalDelay
+        delay: 0.5 + additionalDelay,
       }
     );
   });
@@ -1033,7 +1033,7 @@ function hoverNumberCount() {
         opacity: 1,
         y: "0%",
         duration: 0.6,
-        ease: "power2.out"
+        ease: "power2.out",
       });
 
       gsap.to(countObj, {
@@ -1042,7 +1042,7 @@ function hoverNumberCount() {
         ease: "power2.out",
         onUpdate: () => {
           item.textContent = Math.floor(countObj.val).toLocaleString() + suffix;
-        }
+        },
       });
     };
 
@@ -1051,7 +1051,7 @@ function hoverNumberCount() {
         opacity: 0,
         y: "20%",
         duration: 0.6,
-        ease: "power2.in"
+        ease: "power2.in",
       });
 
       gsap.to(countObj, {
@@ -1060,7 +1060,7 @@ function hoverNumberCount() {
         ease: "power2.in",
         onUpdate: () => {
           item.textContent = Math.floor(countObj.val).toLocaleString() + suffix;
-        }
+        },
       });
     };
 
@@ -1087,7 +1087,7 @@ function scrollToForm() {
         gsap.to(window, {
           duration: 1,
           scrollTo: scrollPosition,
-          ease: "none"
+          ease: "none",
         });
       }
     });
@@ -1107,7 +1107,7 @@ function parallaxIt(e, target, movement) {
     duration: 0.3,
     x: parallaxX,
     y: parallaxY,
-    ease: "power2.out"
+    ease: "power2.out",
   });
 }
 
@@ -1155,7 +1155,7 @@ function hoverIcon() {
         height: rect.height,
         x: 0,
         y: 0,
-        ease: "power2.out"
+        ease: "power2.out",
       });
 
       const img = item.querySelector("img");
@@ -1165,7 +1165,7 @@ function hoverIcon() {
           x: 0,
           y: 0,
           scale: 1,
-          ease: "power2.out"
+          ease: "power2.out",
         });
       }
     });
@@ -1176,7 +1176,7 @@ function hoverIcon() {
         gsap.to(img, {
           duration: 0.3,
           scale: 0.9,
-          ease: "power2.out"
+          ease: "power2.out",
         });
       }
     });
